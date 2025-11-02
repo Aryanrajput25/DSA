11a79c23d47dd5cb1e2e4883be9d87cd1befdc29
Aryanrajput25 <ar83493333@gmail.com>
2025-11-02 16:31:20 +0530

    Time: 4 ms (9.48%), Space: 43.6 MB (5.58%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -5,22 +5,22 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < operations.length; i++) {
-            String op = operations[i];
+            
 
-            if (op.equals("+")) {
+            if (operations[i].equals("+")) {
                 int val1 = stack.pop();
                 int val2 = stack.peek();
                 stack.push(val1);
                 stack.push(val1 + val2);
             } 
-            else if (op.equals("D")) {
+            else if (operations[i].equals("D")) {
                 stack.push(stack.peek() * 2);
             } 
-            else if (op.equals("C")) {
+            else if (operations[i].equals("C")) {
                 stack.pop();
             } 
             else {
-                stack.push(Integer.parseInt(op));
+                stack.push(Integer.parseInt(operations[i]));
             }
         }
 
