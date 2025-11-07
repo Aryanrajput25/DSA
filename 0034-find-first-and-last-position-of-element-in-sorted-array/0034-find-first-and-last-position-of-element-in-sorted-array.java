386827fd7d3ef6866b618a43c1cec17e0edeabce
Aryanrajput25 <ar83493333@gmail.com>
2025-11-07 08:22:48 +0530

    Time: 0 ms (100%), Space: 48.2 MB (5.62%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,53 +1,43 @@
 class Solution {
-    /**
-     * Find the starting and ending position of a given target value in a sorted array.
-     * @param nums - sorted array of integers
-     * @param target - target value to search for
-     * @return array containing [start_index, end_index] of target, or [-1, -1] if not found
-     */
     public int[] searchRange(int[] nums, int target) {
-        // Find the leftmost position where target could be inserted
-        int leftBoundary = findLeftBoundary(nums, target);
-      
-        // Find the leftmost position where (target + 1) could be inserted
-        // This gives us the position right after the last occurrence of target
-        int rightBoundaryPlusOne = findLeftBoundary(nums, target + 1);
-      
-        // If both boundaries are the same, target doesn't exist in the array
-        if (leftBoundary == rightBoundaryPlusOne) {
-            return new int[] {-1, -1};
-        }
-      
-        // Return the range: [leftBoundary, rightBoundaryPlusOne - 1]
-        return new int[] {leftBoundary, rightBoundaryPlusOne - 1};
+        int start = left(nums, target);
+        int end = right(nums, target);
+        return new int[]{start, end};
     }
-  
-    /**
-     * Binary search to find the leftmost position where targetValue could be inserted.
-     * This finds the first occurrence of values >= targetValue.
-     * @param nums - sorted array of integers
-     * @param targetValue - value to search for
-     * @return index of the leftmost position where targetValue should be inserted
-     */
-    private int findLeftBoundary(int[] nums, int targetValue) {
-        int left = 0;
-        int right = nums.length;
-      
-        // Binary search for the leftmost position
-        while (left < right) {
-            // Calculate middle index (using unsigned right shift to avoid overflow)
-            int mid = (left + right) >>> 1;
-          
-            // If mid element is greater than or equal to targetValue,
-            // the answer lies in the left half (including mid)
-            if (nums[mid] >= targetValue) {
-                right = mid;
+
+    private int left(int[] nums, int target) {
+        int index = -1;
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                index = mid;
+                r = mid - 1; 
+            } else if (nums[mid] < target) {
+                l = mid + 1;
             } else {
-                // Otherwise, the answer lies in the right half (excluding mid)
-                left = mid + 1;
+                r = mid - 1;
             }
         }
-      
-        return left;
+        return index;
+    }
+
+    private int right(int[] nums, int target) {
+        int index = -1;
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                index = mid;
+                l = mid + 1; 
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return index;
     }
 }
