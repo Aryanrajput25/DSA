77c974d07c2874ff514ab97dcfa17e949444f5b0
Aryanrajput25 <ar83493333@gmail.com>
2025-12-08 13:05:22 +0530

    Time: 7 ms (83.72%), Space: 46.5 MB (35.13%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -20,11 +20,6 @@ class Solution {
         if(left!=null && right!=null){
             return root;
         } 
-        if(left==null){
-            return right;
-        }
-        else{
-            return left;
-        }
+        return left == null ? right : left;
     }
 }
