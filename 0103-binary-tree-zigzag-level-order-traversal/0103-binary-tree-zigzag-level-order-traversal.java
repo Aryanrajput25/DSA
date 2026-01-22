becc2b88a6fd022af4c7c74517294c3ee31a198c
Aryanrajput25 <ar83493333@gmail.com>
2026-01-22 11:20:50 +0530

    Time: 1 ms (81.96%), Space: 43.8 MB (50.49%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,44 +15,49 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-    List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-    if (root == null) {
-      return result;
-    }
-
-    Deque<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-
-    boolean reverse = false;
-    
-    while (!queue.isEmpty()) {
-      int levelSize = queue.size();
-      List<Integer> currentLevel = new ArrayList<>(levelSize);
-      for (int i=0; i < levelSize; i++) {
-        if (!reverse) {
-          TreeNode currentNode = queue.pollFirst();
-          currentLevel.add(currentNode.val);
-          if (currentNode.left != null) {
-            queue.addLast(currentNode.left);
-          }
-          if (currentNode.right != null) {
-            queue.addLast(currentNode.right);
-          }
-        } else {
-          TreeNode currentNode = queue.pollLast();
-          currentLevel.add(currentNode.val);
-          if (currentNode.right != null) {
-            queue.addFirst(currentNode.right);
-          }
-          if (currentNode.left != null) {
-            queue.addFirst(currentNode.left);
-          }
+        if (root == null) {
+            return result;
         }
-      }
-      reverse = !reverse;
-      result.add(currentLevel);
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean reverse = false;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i=0; i < levelSize; i++) {
+                if(!reverse){
+                    TreeNode currentNode = queue.pollFirst();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.left != null) {
+                        queue.offerLast(currentNode.left);
+                    }
+                    if (currentNode.right != null) {
+                        queue.offerLast(currentNode.right);
+                    }
+                }
+                else{
+                    TreeNode currentNode = queue.pollLast();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.right != null) {
+                        queue.offerFirst(currentNode.right);
+                    }
+                    if (currentNode.left != null) {
+                        queue.offerFirst(currentNode.left);
+                    }
+                    
+                }
+
+            }
+            reverse=!reverse;
+            result.add(currentLevel);
+        }
+        
+        return result;
     }
-    return result;
-  }
+    
+    
 }
