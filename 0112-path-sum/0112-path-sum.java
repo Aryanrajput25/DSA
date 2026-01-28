85ef24667bde0a9344cfdee4825b029c82b70251
Aryanrajput25 <ar83493333@gmail.com>
2026-01-28 15:13:18 +0530

    Time: 0 ms (100%), Space: 44.9 MB (59.96%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -20,7 +20,6 @@ class Solution {
         }
         if(root.val==targetSum && root.left==null && root.right==null){
             return true;
-
         }
         return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
     }
