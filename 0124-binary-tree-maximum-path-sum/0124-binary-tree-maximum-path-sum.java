b8dade049340a1708bf31552b32a91284161177e
Aryanrajput25 <ar83493333@gmail.com>
2026-02-04 22:46:53 +0530

    Time: 1 ms (30.79%), Space: 46.9 MB (32.57%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -23,10 +23,8 @@ class Solution {
         if(node==null){
             return 0;
         }
-        int left=helper(node.left);
-        int right=helper(node.right);
-        left=Math.max(0,left);
-        right=Math.max(0,right);
+        int left=Math.max(0,helper(node.left));
+        int right=Math.max(0,helper(node.right));
         int sum=left+right+node.val;
         ans=Math.max(ans,sum);
         return Math.max(left,right)+node.val;
