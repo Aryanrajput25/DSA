199649873315d20a1ab0932ba153da86bb1f19c5
Aryanrajput25 <ar83493333@gmail.com>
2026-04-27 20:24:57 +0530

    Time: 35 ms (43.23%), Space: 58.7 MB (92.42%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,32 +2,21 @@ class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
         int n=arr.length;
         Arrays.sort(arr);
-        // Store final result
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // First loop for first element
-        for (int i = 0; i < n; i++) {
-            // Skip duplicates for first element
-            if (i > 0 && arr[i] == arr[i - 1]) continue;
-
-            // Two pointers
-            int left = i + 1, right = n - 1;
-
-            // Find pairs for current arr[i]
-            while (left < right) {
-                int sum = arr[i] + arr[left] + arr[right];
-
-                if (sum == 0) {
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(i>0 && arr[i]==arr[i-1]) continue;
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=arr[i]+arr[left]+arr[right];
+                if(sum==0){
                     ans.add(Arrays.asList(arr[i], arr[left], arr[right]));
                     left++;
                     right--;
-
-                    // Skip duplicates for left
-                    while (left < right && arr[left] == arr[left - 1]) left++;
-                    // Skip duplicates for right
-                    while (left < right && arr[right] == arr[right + 1]) right--;
+                    while(left<right && arr[left]==arr[left-1]) left++;
+                    while(left<right && arr[right]==arr[right+1]) right--;
                 }
-                else if (sum < 0) left++;
+                else if(sum<0) left++;
                 else right--;
             }
         }
