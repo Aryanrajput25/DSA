073e3bcc7bd83ad3c89a7bcf85cd8d3b49d904a4
Aryan Rajput <ar83493333@gmail.com>
2026-06-07 17:06:54 +0530

    Time: 6 ms (71.96%), Space: 45 MB (86.28%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> ans=new ArrayList<>();
+        func(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+    public void func(int idx,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
+        if(target==0){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=idx;i<arr.length;i++){
+            if(i!=idx && arr[i]==arr[i-1]) continue;
+            if(arr[i]>target) break;
+            ds.add(arr[i]);
+            func(i+1,arr,target-arr[i],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
