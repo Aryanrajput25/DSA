8207535ddb07efbc338445f92e655e7b3ae36f81
Aryan Rajput <ar83493333@gmail.com>
2026-07-25 22:00:25 +0530

    Time: 1 ms (98.02%), Space: 47.4 MB (81.08%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,46 +1,33 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length;
-        int n= matrix[0].length;
-        Boolean row=false;
-        Boolean col=false;
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]==0){
-                row=true;
-                break;
-            }
-        }
+        int col0=1;
+        int m=matrix[0].length;
+        int n=matrix.length;
         for(int i=0;i<n;i++){
-            if(matrix[0][i]==0){
-                col=true;
-                break;
-            }
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
+            for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
                     matrix[i][0]=0;
-                    matrix[0][j]=0;
+                    if(j!=0) matrix[0][j]=0;
+                    else col0=0;
                 }
             }
         }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j]=0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
                 }
             }
         }
-        if(row){
-            for(int i=0;i<m;i++){
+        if(matrix[0][0]==0){
+            for(int j=0;j<m;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<n;i++){
                 matrix[i][0]=0;
             }
         }
-        if(col){
-            for(int i=0;i<n;i++){
-                matrix[0][i]=0;
-            }
-        }
     }
-    
 }
